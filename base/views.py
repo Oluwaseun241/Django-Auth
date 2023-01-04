c1b238cc3d682fe7c44004f30958e921a1eadea5
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-01-04 15:24:27 +0100

    init

---
+++ base/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+
+
+def login(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+
+    context = {'page': page}
+    return render(request, 'base/home.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def register(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+    return render(request, 'base/register_login.html', {'form': form})
+
+
+
+def home(request):
+    return render(request, 'base/home.html')
